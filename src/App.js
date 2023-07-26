365d8979a9cb24eada5f280582886474a437540b
Michal Owsiak <micowsiak@gmail.com>
2023-07-26 22:41:00 +0200

    calculating functionality added

--- src/App.js
+++ src/App.js
@@ -3,15 +3,44 @@ import Form from './Form';
 import Header from './Header';
 import Footer from './Footer';
 import Container from './Container';
+import { useState } from 'react';
+import { currencies } from './Form/currencies';
+
+
 
 function App() {
+  const [result, setResult] = useState();
+
+  const calculateResult = (currencyFrom, currencyTo, amount) => {
+    const rateFrom = currencies.find(({ name }) => name === currencyFrom).rate
+    const rateTo = currencies.find(({ name }) => name === currencyTo).rate
+
+    setResult({
+      sourceAmount: +amount,
+      calculatedAmount: (rateFrom / rateTo) * amount,
+      currencyFrom,
+      currencyTo,
+    });
+  };
+
   return (
     <main>
-      <Header title="currency converter" />
-      <Container content={<Form />} />
-      <Footer info="Rates calculated based on data as of 26.07.2023" />
+      <Header 
+        title="currency converter" 
+      />
+      <Container 
+        content={
+          <Form 
+            result={result}
+            calculateResult={calculateResult}
+          />
+        } 
+      />
+      <Footer
+        info="Rates calculated based on data as of 26.07.2023" 
+      />
     </main>
   );
-}
+};
 
 export default App;
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,96 +1,116 @@
 import "./style.css";
+import { useState } from 'react';
+import { currencies } from "./currencies.js";
+import Result from "./Result";
 
-const Form = () => (
-  <form className="form">
-    <fieldset className="form__fieldset">
-      <p>
-        <label>
-          <div className="form__amountContainer">
-            <span className="form__labelText">
-              Amount
-            </span>
-            <input
-              className="form__amountField"
-              type="number"
-              name="amount"
-              step="0.01"
-              min="0"
-              placeholder="0.00"
-            />
-          </div>
-        </label>
-      </p>
-      <p>
-        <label>
-          <div className="form__currenciesContainer">
-            <span className="form__labelText form__labelText--currencies">
-              From
-            </span>
-            <select className="form__currencyField"
-              name="currencyFrom"
-            >
-              <option selected>PLN</option>
-              <option>EUR</option>
-              <option>USD</option>
-              <option>CHF</option>
-              <option>GBP</option>
-              <option>JPY</option>
-            </select>
-          </div>
-        </label>
-      </p>
-      <p>
-        <label>
-          <div className="form__currenciesContainer">
-            <span className="form__labelText form__labelText--currencies">
-              To
-            </span>
-            <select className="form__currencyField"
-              name="currencyTo"
-            >
-              <option>PLN</option>
-              <option selected>EUR</option>
-              <option>USD</option>
-              <option>CHF</option>
-              <option>GBP</option>
-              <option>JPY</option>
-            </select>
-          </div>
-        </label>
-      </p>
-      <p className="form__currencyRate">
-        <span>
-          1 PLN
-        </span> =
-        <span>
-          0.22 EUR
-        </span>
-      </p>
-      <div className="form__result">
+const Form = ({ calculateResult, result }) => {
+  const [currencyFrom, setCurrencyFrom] = useState(currencies[0].name);
+  const [currencyTo, setCurrencyTo] = useState(currencies[1].name);
+  const [amount, setAmount] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    calculateResult (currencyFrom, currencyTo, amount)
+  };
+
+  return (
+    <form className="form" onSubmit={onSubmit}>
+      <fieldset className="form__fieldset">
         <p>
-          <span className="form__result--from">
-            0.00
+          <label>
+            <div className="form__amountContainer">
+              <span className="form__labelText">
+                Amount
+              </span>
+              <input
+                value={amount}
+                onChange={({ target }) => setAmount(target.value)}
+                className="form__amountField"
+                type="number"
+                name="amount"
+                step="0.01"
+                min="0"
+                placeholder="0.00"
+              />
+            </div>
+          </label>
+        </p>
+        <p>
+          <label>
+            <div className="form__currenciesContainer">
+              <span className="form__labelText form__labelText--currencies">
+                From
+              </span>
+              <select
+                className="form__currencyField"
+                name="currencyFrom"
+                value={currencyFrom.name}
+                onChange={({ target }) => setCurrencyFrom(target.value)}
+              >
+                {currencies.map((currencyFrom => (
+                  <option
+                    key={currencyFrom.name}
+                  >
+                    {currencyFrom.name}
+                  </option>
+                )))}
+              </select>
+            </div>
+          </label>
+        </p>
+        <p>
+          <label>
+            <div className="form__currenciesContainer">
+              <span className="form__labelText form__labelText--currencies">
+                To
+              </span>
+              <select
+                className="form__currencyField"
+                name="currencyTo"
+                value={currencyTo.name}
+                defaultValue="EUR"
+                onChange={({ target }) => setCurrencyTo(target.value)}
+              >
+                {currencies.map((currencyTo => (
+                  <option
+                    key={currencyTo.name}
+                  >
+                    {currencyTo.name}
+                  </option>
+                )))}
+              </select>
+            </div>
+          </label>
+        </p>
+        <p className="form__currencyRate">
+          <span>
+            1 PLN
+          </span> =
+          <span>
+            0.22 EUR
           </span>
-          <span className="form__result--to" />
         </p>
-      </div>
-      <div className="form__buttonsContainer">
-        <p>
-          <button className="form__button">
-            Convert
-          </button>
-        </p>
-        <p>
-          <button
-            type="reset"
-            className="form__button form__button--reset"
-          >
-            Reset
-          </button>
-        </p>
-      </div>
-    </fieldset>
-  </form>
-);
+        <Result result={result}/>
+        <div className="form__buttonsContainer">
+          <p>
+            <button 
+              className="form__button"
+            >
+              Convert
+            </button>
+          </p>
+          <p>
+            <button
+              type="reset"
+              className="form__button form__button--reset"
+            >
+              Reset
+            </button>
+          </p>
+        </div>
+      </fieldset>
+    </form>
+  );
+};
 
 export default Form;
