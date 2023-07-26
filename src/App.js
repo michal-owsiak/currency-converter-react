a6c32d47440d9834eae303fbc3310c9c9866370c
Michal Owsiak <micowsiak@gmail.com>
2023-07-26 16:43:54 +0200

    icon & images update

--- src/App.js
+++ src/App.js
@@ -7,15 +7,9 @@ import Container from './Container';
 function App() {
   return (
     <main>
-      <Header
-        title="currency converter"
-      />
-      <Container
-        content={<Form />}
-      />
-      <Footer
-        info="Rates calculated based on data as of 14.04.2023"
-      />
+      <Header title="currency converter" />
+      <Container content={<Form />} />
+      <Footer info="Rates calculated based on data as of 14.04.2023" />
     </main>
   );
 }
