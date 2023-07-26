74575a07de899344b0adacfbeedb712334dc0c36
Michal Owsiak <micowsiak@gmail.com>
2023-07-26 22:32:09 +0200

    Result component

---
+++ src/Form/Result/index.js
@@ -0,0 +1,18 @@
+import "./style.css"
+
+const Result = ({ result }) => (
+  <p className="result">
+    {result !== undefined && (
+      <>
+        <span className="result--from">
+          {result.sourceAmount.toFixed(2)} {result.currencyFrom} = {""}
+        </span>
+        <span className="result--to">
+          {result.calculatedAmount.toFixed(2)} {result.currencyTo}
+        </span>
+      </>
+    )}
+  </p>
+);
+
+export default Result;
