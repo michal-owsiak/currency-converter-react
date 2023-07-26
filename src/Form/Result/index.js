bcaeb2fbfa6981546bf66291d44ffe86b0bcfcc8
Michal Owsiak <micowsiak@gmail.com>
2023-07-26 23:25:08 +0200

    default result set

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -2,17 +2,24 @@ import "./style.css"
 
 const Result = ({ result }) => (
   <p className="result">
-  {result !== undefined && (
-    <>
-      <span className="result--from">
-        {result.sourceAmount.toFixed(2)} {result.currencyFrom} = {}
-      </span>
-      <span className="result--to">
-        {result.calculatedAmount.toFixed(2)} {result.currencyTo}
-      </span>
-    </>
-  )}
-</p>
+    {result === undefined && (
+      <>
+        <span className="result--from">
+          0.00
+        </span>
+      </>
+    )}
+    {result !== undefined && (
+      <>
+        <span className="result--from">
+          {result.sourceAmount.toFixed(2)} {result.currencyFrom} = { }
+        </span>
+        <span className="result--to">
+          {result.calculatedAmount.toFixed(2)} {result.currencyTo}
+        </span>
+      </>
+    )}
+  </p>
 );
 
 export default Result;
