13d61cf3464649a3459a8eaa7b6fb59e43a19c05
Michal Owsiak <micowsiak@gmail.com>
2023-07-27 15:56:19 +0200

    <p> nesting problem solved, css updated

--- src/Form/index.js
+++ src/Form/index.js
@@ -32,79 +32,73 @@ const Form = ({ calculateRate, calculateResult, initResult, result }) => {
   return (
     <form className="form" onSubmit={onSubmit}>
       <fieldset className="form__fieldset">
-        <p>
-          <label>
-            <div className="form__amountContainer">
-              <span className="form__labelText">
-                Amount
-              </span>
-              <input
-                value={amount}
-                onChange={({ target }) => setAmount(target.value)}
-                className="form__amountField"
-                type="number"
-                name="amount"
-                step="0.01"
-                min="0"
-                placeholder="0.00"
-              />
-            </div>
-          </label>
-        </p>
-        <p>
-          <label>
-            <div className="form__currenciesContainer">
-              <span className="form__labelText form__labelText--currencies">
-                From
-              </span>
-              <select
-                className="form__currencyField"
-                name="currencyFrom"
-                value={currencyFrom}
-                onChange={({ target }) => setCurrencyFrom(target.value)}
-              >
-                {currencies.map((currency => (
-                  <option
-                    key={currency.name}
-                    value={currency.name}
-                  >
-                    {currency.name}
-                  </option>
-                )))}
-              </select>
-            </div>
-          </label>
-        </p>
-        <p>
-          <label>
-            <div className="form__currenciesContainer">
-              <span className="form__labelText form__labelText--currencies">
-                To
-              </span>
-              <select
-                className="form__currencyField"
-                name="currencyTo"
-                defaultValue="EUR"
-                onChange={({ target }) => setCurrencyTo(target.value)}
-              >
-                {currencies.map((currency => (
-                  <option
-                    key={currency.name}
-                    value={currency.name}
-                  >
-                    {currency.name}
-                  </option>
-                )))}
-              </select>
-            </div>
-          </label>
-        </p>
+        <label>
+          <div className="form__amountContainer">
+            <span className="form__labelText">
+              Amount
+            </span>
+            <input
+              value={amount}
+              onChange={({ target }) => setAmount(target.value)}
+              className="form__amountField"
+              type="number"
+              name="amount"
+              step="0.01"
+              min="0"
+              placeholder="0.00"
+            />
+          </div>
+        </label>
+        <label>
+          <div className="form__currenciesContainer">
+            <span className="form__labelText form__labelText--currencies">
+              From
+            </span>
+            <select
+              className="form__currencyField"
+              name="currencyFrom"
+              value={currencyFrom}
+              onChange={({ target }) => setCurrencyFrom(target.value)}
+            >
+              {currencies.map((currency => (
+                <option
+                  key={currency.name}
+                  value={currency.name}
+                >
+                  {currency.name}
+                </option>
+              )))}
+            </select>
+          </div>
+        </label>
+        <label>
+          <div className="form__currenciesContainer">
+            <span className="form__labelText form__labelText--currencies">
+              To
+            </span>
+            <select
+              className="form__currencyField"
+              name="currencyTo"
+              defaultValue="EUR"
+              onChange={({ target }) => setCurrencyTo(target.value)}
+            >
+              {currencies.map((currency => (
+                <option
+                  key={currency.name}
+                  value={currency.name}
+                >
+                  {currency.name}
+                </option>
+              )))}
+            </select>
+          </div>
+        </label>
         <Rate
           currencyFrom={currencyFrom}
           currencyTo={currencyTo}
           rate={rate}
         />
-        <Result 
+        <Result
           result={result}
         />
         <div className="form__buttonsContainer">
