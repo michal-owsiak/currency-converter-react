0d7f90c9849889273f4aabc6d4b4e61433f557df
Michal Owsiak <micowsiak@gmail.com>
2023-08-13 14:03:03 +0200

    formatting

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -7,20 +7,20 @@ export const Fieldset = styled.fieldset`
   padding: 16px 13% 10px;
 
 	@media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-    padding: 16px 30px 10px;
+		padding: 16px 30px 10px;
 	}
 `;
 
 export const Container = styled.div`
-  padding: 24px 0 24px;
-  display: grid;
-  grid-template-columns: 80px 1fr;
-  grid-gap: 34px;
+	padding: 24px 0 24px;
+	display: grid;
+	grid-template-columns: 80px 1fr;
+	grid-gap: 34px;
 
 	@media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-    grid-template-columns: 1fr;
-    grid-gap: 16px;
-  }
+		grid-template-columns: 1fr;
+		grid-gap: 16px;
+	}
 `;
 
 export const CurrenciesContainer = styled(Container)`
@@ -29,15 +29,15 @@ export const CurrenciesContainer = styled(Container)`
 
 export const LabelText = styled.span`
 	font-size: 11px;
-  font-weight: 500;
-  color: ${({ theme }) => theme.color.scorpion};
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+	font-weight: 500;
+	color: ${({ theme }) => theme.color.scorpion};
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
 
 	@media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-  	text-align: center;
-  }
+		text-align: center;
+	}
 `;
 
 export const Input = styled.input`
@@ -60,25 +60,25 @@ export const Input = styled.input`
 
 export const Select = styled.select`
 	border: 1.5px solid ${({ theme }) => theme.color.silver};
-  border-radius: 6px;
-  width: 100%;
-  padding: 10px;
-  font-size: 13px;
-  font-weight: 600;
-  color: ${({ theme }) => theme.color.tundora};
-  background: ${({ theme }) => theme.color.white};
-  outline: ${({ theme }) => theme.color.dodgerBlue};
-  text-align: center;
+	border-radius: 6px;
+	width: 100%;
+	padding: 10px;
+	font-size: 13px;
+	font-weight: 600;
+	color: ${({ theme }) => theme.color.tundora};
+	background: ${({ theme }) => theme.color.white};
+	outline: ${({ theme }) => theme.color.dodgerBlue};
+	text-align: center;
 
 	&:hover {
 		background-color: ${({ theme }) => theme.color.zircon};
-  	border: 1.5px solid ${({ theme }) => theme.color.dodgerBlue};
-  	transition: all 0.5s ease;
+		border: 1.5px solid ${({ theme }) => theme.color.dodgerBlue};
+		transition: all 0.5s ease;
 	}
 
   @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-    padding: 12px 12px 12px 24px;
-  }
+		padding: 12px 12px 12px 24px;
+	}
 `;
 
 export const ButtonsContainer = styled.div`
@@ -87,17 +87,17 @@ export const ButtonsContainer = styled.div`
 
 export const Button = styled.button`
 	display: block;
-  cursor: pointer;
-  width: 90%;
-  height: 55px;
-  color: ${({ theme }) => theme.color.white};
-  border: 1.5px solid ${({ theme }) => theme.color.blueRibbon};
-  border-radius: 50px;
-  background: ${({ theme }) => theme.color.blueRibbon};
-  font-weight: 600;
-  font-size: 13px;
-  margin: 12px auto;
-  height: 48px;
+	cursor: pointer;
+	width: 90%;
+	height: 55px;
+	color: ${({ theme }) => theme.color.white};
+	border: 1.5px solid ${({ theme }) => theme.color.blueRibbon};
+	border-radius: 50px;
+	background: ${({ theme }) => theme.color.blueRibbon};
+	font-weight: 600;
+	font-size: 13px;
+	margin: 12px auto;
+	height: 48px;
 	
 	&:hover {
 		background: ${({ theme }) => theme.color.cobalt};
@@ -106,17 +106,17 @@ export const Button = styled.button`
 	}
 
   @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-    width: 100%;
+		width: 100%;
   }
 `;
 
 export const ResetButton = styled(Button)`
-  color: ${({ theme }) => theme.color.dodgerBlue};
-  background: ${({ theme }) => theme.color.white};
-  font-weight: 700;
-  transition: all 0.5s ease;
+	color: ${({ theme }) => theme.color.dodgerBlue};
+	background: ${({ theme }) => theme.color.white};
+	font-weight: 700;
+	transition: all 0.5s ease;
 
-  &:hover {
-    color: ${({ theme }) => theme.color.white};
-  }
+	&:hover {
+		color: ${({ theme }) => theme.color.white};
+	}
 `;
